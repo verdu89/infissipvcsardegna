92fe2ce2fa1955b0dbe95ceefbd4ee49914a5fc5
verdu89 <verdu89@gmail.com>
2020-03-21 15:31:12 +0100

    aggiunto modal per COVID

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function( $ ) {
 
+  $('#myModalAnnounce').modal('show');
+
   // Header fixed and Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
