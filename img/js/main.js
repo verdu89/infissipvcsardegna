6729d0254e83d083eaa206525be2181a8d6067f5
verdu89 <verdu89@gmail.com>
2020-11-02 18:05:53 +0100

    online

---
+++ img/js/main.js
@@ -0,0 +1,248 @@
+jQuery(document).ready(function( $ ) {
+
+  // Header fixed and Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      $('#header').addClass('header-fixed');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      $('#header').removeClass('header-fixed');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+
+  });
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {opacity:'show'},
+    speed: 400
+  });
+
+  // Mobile Navigation
+  if( $('#nav-menu-container').length ) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
+    $mobile_nav.find('> ul').attr({ 'class' : '', 'id' : '' });
+    $('body').append( $mobile_nav );
+    $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
+    $('body').append( '<div id="mobile-body-overly"></div>' );
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function(e){
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function(e){
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+      $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function (e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+       if ( $('body').hasClass('mobile-nav-active') ) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  // Smoth scroll on page hash links
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if( $('#header').length ) {
+          top_space = $('#header').outerHeight();
+
+          if( ! $('#header').hasClass('header-fixed') ) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ( $(this).parents('.nav-menu').length ) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ( $('body').hasClass('mobile-nav-active') ) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    }
+  });
+
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+
+  // For use with dynamic Google maps
+  if ($('#google-map').length) {
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+      var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+      var mapOptions = {
+        zoom: 14,
+        scrollwheel: false,
+        center: myLatlng
+      };
+      var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+      });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+  }
+
+
+  //Custom code
+
+  // cambia immagine SCUDO
+  changeImage();
+
+
+  // Modal Coronavirus (COVID-19)
+  /*$('#myModalAnnounce').modal('show');*/
+
+  // Modal Allega File
+/*  $('#allegaButton').click(function() {
+
+    $('#modalAttachFile').modal('show');
+    $('#allegaButton').addClass('hide');
+
+
+  });*/
+
+
+  //Modal Youtube
+  $("#myModal").on('hidden.bs.modal', function (e) {
+    $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+  });
+
+
+});
+
+// Custum code
+
+
+ // Post form
+
+function doSubmit() {
+
+  let scriptUrl = "https://script.google.com/macros/s/AKfycbz-XnpWOP1-v2cosUGetAKXOghnP0EocCVQXy-7zA/exec";
+  let formData = {
+    name: $('#name').val(),
+    email: $('#email').val(),
+    phone: $('#phone').val(),
+    place: $('#place').val(),
+    message: $('#message').val()
+  };
+
+  $.ajax({
+    type: "POST",
+    url: scriptUrl,
+    data: formData,
+    crossDomain: true,
+    error: function (data) {
+      setThanksButton();
+
+    },
+    success: function (data) {
+      setThanksButton();
+
+
+    }
+
+  });
+
+}
+
+function setThanksButton() {
+  let button = $('#sendMessageButton');
+  button.removeClass('.block');
+  button.addClass('blockSuccess');
+  button.text('Messaggio inviato');
+  $(window.alert('Messaggio inviato correttamente'));
+  $('#name').val('') ;
+  $('#email').val('');
+  $('#phone').val('');
+  $('#message').val('');
+  $('#place').val('');
+  $('#allegaButton').val('');
+  $('#checkboxForm').prop('checked', false);
+
+
+}
+
+function  changeImage() {
+  let image = $('#news')
+  let interval = window.setInterval(function () {
+        if (image.attr("src") === "img/news.png"){
+          $("#news").attr("src", "img/newsLamp.png")
+
+        }else {$("#news").attr("src", "img/news.png")}
+  }, 1000)
+
+
+}
+
+
+
+
+
+
+
+
+
